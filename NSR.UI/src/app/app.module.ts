5a03be7702ce8d6525bcb45b9efc83e0b0655cc3
Arzhan <chilchinov99@mail.ru>
2024-06-05 15:11:02 +0300

    add ui with map

---
+++ NSR.UI/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MapComponent } from './components/map/map.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    LeafletModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
