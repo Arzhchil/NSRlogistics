b036e2aa3c10b4c0109876f4b17dcb4f604fe4a7
dev_danil <tinder12347@mail.ru>
2024-06-07 20:51:06 +0300

    backend 1) миграции создания сущностей корабля и типа корабля

---
+++ backend/backend/Migrations/20240607175025_AddShipAndShipClass.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShipAndShipClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShipClass",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ShipType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipClass", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ship",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ShipName = table.Column<string>(type: "text", nullable: false),
+                    ShipClassId = table.Column<int>(type: "integer", nullable: false),
+                    Speed = table.Column<double>(type: "double precision", nullable: false),
+                    TypeIceClass = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ship", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ship_ShipClass_ShipClassId",
+                        column: x => x.ShipClassId,
+                        principalTable: "ShipClass",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ship_ShipClassId",
+                table: "Ship",
+                column: "ShipClassId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ship");
+
+            migrationBuilder.DropTable(
+                name: "ShipClass");
+        }
+    }
+}
---
+++ backend/backend/Models/Ship.cs
@@ -0,0 +1,36 @@
+﻿namespace backend.Models
+{
+    /// <summary>
+    /// Параметры корабля
+    /// </summary>
+    public class Ship
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя корабля
+        /// </summary>
+        public string ShipName { get; set; } = null!;
+
+        /// <summary>
+        /// Отношение к классу корабля
+        /// </summary>
+        public ShipClass ShipClass { get; set; } = null!;
+
+        /// <summary>
+        /// Скорость
+        /// </summary>
+        public double Speed { get; set; }
+
+        /// <summary>
+        /// Тип проходимости
+        /// </summary>
+        public TypeIceClass TypeIceClass { get; set; }
+    }
+    public enum TypeIceClass
+    {
+        Class1,
+        Class2,
+        Class3
+    }
+}
