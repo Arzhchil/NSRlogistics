92abf2f60c1a107545cf0719203f5bcca45bf209
dev_danil <tinder12347@mail.ru>
2024-06-08 18:33:09 +0300

    backend 1) добален сервис парсинга файла 2) теперь загружать можно только файлы формата ".xslx" 3) создано котроллер парсера(нахуй не нужен, ток для тестов) 4) добавлена либа Aspose.Cells для работы с excel

---
+++ backend/backend/Controllers/ParseDataController.cs
@@ -0,0 +1,33 @@
+﻿using backend.DTOs;
+using backend.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    /// <summary>
+    /// НУЖЕН ТОЛЬКО ДЛЯ ТЕСТОВ ПАРСИНГА
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParseDataController : ControllerBase
+    {
+        private readonly IFileParser _fileParser;
+        public ParseDataController(IFileParser fileParser)
+        {
+            _fileParser = fileParser;
+        }
+
+        /// <summary>
+        /// парсинг файла(нужен для тестов, так то нахуй не сдался)
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("/parseData")]
+        public async Task<IActionResult> LoadFile([FromBody] ParserDTO parserDTO)
+        {
+            int[,] parseData = await _fileParser.FileParser(parserDTO);
+
+            return Ok(parseData);
+        }
+    }
+}
